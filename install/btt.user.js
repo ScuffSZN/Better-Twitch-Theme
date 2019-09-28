a89c0e0cc05755646bf454b7dbff21576cbb2e29
Scuffed <45866296+ScuffedDev@users.noreply.github.com>
2019-09-28 17:27:22 -0400

    upload script

---
+++ install/btt.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Better Twitch Theme
+// @namespace    http://scuffed.dev/Better-Twitch-Theme
+// @version      0.1
+// @description  Better Twitch Theme
+// @author       ScuffedDev
+// @match        https://*.twitch.tv/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            console.log('[Better Twitch Theme] added!')
+            addStyleString(xhr.responseText);
+        }
+    }
+    xhr.open('GET', 'https://scuffed.dev/assets/theme.css', true);
+    xhr.send(null);
+})();
+
+function addStyleString(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+}
